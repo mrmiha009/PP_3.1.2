3b68f043d51690ee3bb5d6241f817f6a801428aa
mrmiha009 <pfc.cska.rfs@ya.ru>
2023-04-12 11:11:48 +0300

    исправлено

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,13 +5,17 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
-import web.service.CarServiceImpl;
 
 
 @Controller
 public class CarController {
 
-    private final CarService carService = new CarServiceImpl();
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
 
     @GetMapping("/cars")
     public String carsList(@RequestParam(value = "count", defaultValue = "5") int count, ModelMap modelMap) {
